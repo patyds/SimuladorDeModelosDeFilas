bc07d083fb14a527d1e7fdb7bf3949db3a82e98a
Patricia Dávila <pattyds.1796@gmail.com>
2019-05-04 20:14:24 -0500

    cost simulation

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -4,6 +4,10 @@ const superagent = require('superagent');
 $(document).ready(function() {
     $(".simulator-tabs-names .tab-name").click(selectTab);
     $(".distribution").click(erlang);
+    $(".mm1-cost").click(mm1cost);
+    $(".mms-cost").click(mmscost);
+    $(".mmsk-cost").click(mmskcost);
+    $(".mg1-cost").click(mg1cost);
     $("#mm1-button").click(mm1);
     $("#mms-button").click(mms);
     $("#mmsk-button").click(mmsk);
@@ -45,4 +49,36 @@ function mmsk(){
 
 function mg1(){
     
+}
+
+function mm1cost(){
+    if($('input[name=mm1-cost]').is(':checked')){
+        $('.mm1-cost-input').show()
+    }else{
+        $('.mm1-cost-input').hide()
+    }
+}
+
+function mmskcost(){
+    if($('input[name=mmsk-cost]').is(':checked')){
+        $('.mmsk-cost-input').show()
+    }else{
+        $('.mmsk-cost-input').hide()
+    }
+}
+
+function mmscost(){
+    if($('input[name=mms-cost]').is(':checked')){
+        $('.mms-cost-input').show()
+    }else{
+        $('.mms-cost-input').hide()
+    }
+}
+
+function mg1cost(){
+    if($('input[name=mg1-cost]').is(':checked')){
+        $('.mg1-cost-input').show()
+    }else{
+        $('.mg1-cost-input').hide()
+    }
 }
