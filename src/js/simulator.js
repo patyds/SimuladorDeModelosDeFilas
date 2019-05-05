74e3c66a3eec2a75b132a2692171478f2726ba16
Patricia Dávila <pattyds.1796@gmail.com>
2019-05-04 22:32:53 -0500

    mm1

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 const superagent = require('superagent');
+const app = "https://mononucleo-modelos-filas.herokuapp.com/";
 
 $(document).ready(function() {
     $(".simulator-tabs-names .tab-name").click(selectTab);
@@ -36,7 +37,93 @@ function erlang(){
 }
 
 function mm1(){
-
+    $("#tbody-mg1-w").html("");
+    if($("#mm1-arrival").val()=="" || $("#mm1-service").val()==""){
+        alert("Por favor llene los campos requeridos")
+    }else if(parseInt($("#mm1-arrival").val()) < 0 || parseInt($("#mm1-service").val()) < 0){
+        alert("Por favor, ingrese números positivos")
+    }else{
+        if(navigator.onLine){
+            if( $('input[name=mm1-cost]').is(':checked')){
+                if($('#mm1-cs').val() == "" || $('#mm1-cw').val()== ""){
+                    alert("por favor, ingresa los valores para los costos");
+                }else{
+                    var probss = $("#mm1-prob").val() != "" ? $("#mm1-prob").val(): "";
+                    superagent.get(app+'mm1')
+                    .query({ lambd: $("#mm1-arrival").val(), mu: $("#mm1-service").val(), probs : probss, twc: $("#mm1-cw").val(), sc: $("#mm1-cs").val() })
+                    .end((err, res) => {
+                        if (err) { 
+                            return console.log(err); 
+                        }else{
+                            var mm1 = res.body;
+                            console.log(res.body);
+                            $("#tdata-mm1-w").text(mm1.w);
+                            $("#tdata-mm1-wq").text(mm1.wq);
+                            $("#tdata-mm1-l").text(mm1.l);
+                            $("#tdata-mm1-lq").text(mm1.lq);
+                            $("#tbody-mm1-p0").html(mm1.p0);
+                            /*var resbody =$("#tbody-ms");
+                            if(ms.Randoms.length < $("#ms-lim").val()){
+                                alert("El periodo se cumplió antes del límite de números random")
+                            }
+                            for (let index = 0; index < ms.Randoms.length; index++) {
+                                var row = $("<div></div>").addClass("trow")
+                                var element = $("<div></div>").addClass("tdata number").text(index+1);
+                                $(row).append(element);
+                                element = $("<div></div>").addClass("tdata").text(ms.Seed[index]);
+                                $(row).append(element);
+                                element = $("<div></div>").addClass("tdata").text(ms.Generator[index]);
+                                $(row).append(element);
+                                element = $("<div></div>").addClass("tdata").text(ms.Generated[index]);
+                                $(row).append(element);
+                                element = $("<div></div>").addClass("tdata").text(ms.Randoms[index].toFixed(4));
+                                $(row).append(element);
+                                $(resbody).append(row);
+                            }*/
+                            
+                        }
+                    });
+                }
+            }else{
+                var probss = $("#mm1-prob").val() != "" ? $("#mm1-prob").val(): "" ;
+                superagent.get(app+'mm1')
+                .query({ lambd: $("#mm1-arrival").val(), mu: $("#mm1-service").val(), probs : probss })
+                .end((err, res) => {
+                    if (err) { 
+                        return console.log(err); 
+                    }else{
+                        var mm1 = res.body;
+                        console.log(res.body);
+                        $("#tdata-mm1-w").text(mm1.w);
+                        $("#tdata-mm1-wq").text(mm1.wq);
+                        $("#tdata-mm1-l").text(mm1.l);
+                        $("#tdata-mm1-lq").text(mm1.lq);
+                        $("#tdata-mm1-p0").html(mm1.p0);
+                        /*var resbody =$("#tbody-ms");
+                        if(ms.Randoms.length < $("#ms-lim").val()){
+                            alert("El periodo se cumplió antes del límite de números random")
+                        }
+                        for (let index = 0; index < ms.Randoms.length; index++) {
+                            var row = $("<div></div>").addClass("trow")
+                            var element = $("<div></div>").addClass("tdata number").text(index+1);
+                            $(row).append(element);
+                            element = $("<div></div>").addClass("tdata").text(ms.Seed[index]);
+                            $(row).append(element);
+                            element = $("<div></div>").addClass("tdata").text(ms.Generator[index]);
+                            $(row).append(element);
+                            element = $("<div></div>").addClass("tdata").text(ms.Generated[index]);
+                            $(row).append(element);
+                            element = $("<div></div>").addClass("tdata").text(ms.Randoms[index].toFixed(4));
+                            $(row).append(element);
+                            $(resbody).append(row);
+                        }*/
+                    }
+                });
+            }
+        }else{
+            alert("Lo sentimos, necesitas tener una conexión a internet para usar el generador")
+        }
+    }
 }
 
 function mms(){
